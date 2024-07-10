b068392fcfc6ffbafe5376ee39f21aebc6e331e3
iballidev <iballi.dev@gmail.com>
2024-07-10 16:58:04 +0100

    update*

--- src/app_api/routes/index.ts
+++ src/app_api/routes/index.ts
@@ -5,7 +5,7 @@ import { handle_Login } from '../handlers/auth.handler';
 import { handle_Logout } from '../handlers/logout.handler';
 import { handle_refreshToken } from '../handlers/refresh-token.handler';
 import { handle_DeleteCategoryCategoryId, handle_GetCategoryList, handle_PostCategory } from '../handlers/category.handler';
-import { userCredentialsFromRefeshToken } from '../../middlewares/user-credentials-from-refresh-token';
+import { editorCredentialsFromRefeshToken } from '../../middlewares/editor-credentials-from-refresh-token';
 // import logger from '../utils/logger';
 // import {healthCheck} from '../handlers/healthCheck'
 
@@ -19,12 +19,15 @@ router.route('/auth/refresh-token').get(handle_refreshToken);
 
 /* POST Blogpost. */
 // router.post('/', handle_PostBlog);
-router.route('/blog-post').post(userCredentialsFromRefeshToken, handle_PostBlog).get(handle_GetAllPosts);
-router.route('/blog-post/:postId').get(handle_GetPostById).put(userCredentialsFromRefeshToken, handle_UpdatePostById).delete(handle_DeletePostById);
+router.route('/blog-post').post(editorCredentialsFromRefeshToken, handle_PostBlog).get(editorCredentialsFromRefeshToken, handle_GetAllPosts);
+router
+  .route('/blog-post/:postId')
+  .get(handle_GetPostById)
+  .put(editorCredentialsFromRefeshToken, handle_UpdatePostById)
+  .delete(editorCredentialsFromRefeshToken, handle_DeletePostById);
 
 /**Blog Category routes */
 router.route('/post-category').post(handle_PostCategory).get(handle_GetCategoryList);
 router.route('/post-category/:categoryId').delete(handle_DeleteCategoryCategoryId);
-
 
 export default router;

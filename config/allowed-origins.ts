fd147e8664a92021c078c81ff26217cb3eddc147
iballidev <iballi.dev@gmail.com>
2024-07-20 02:33:16 +0100

    code files restructure

--- config/allowed-origins.ts
+++ config/allowed-origins.ts
@@ -3,6 +3,7 @@ const allowedOrigins: string[] = [
   'http://localhost:3000',
   'http://localhost:4200',
   'https://guru-sys-blog-frontend.vercel.app',
+  'https://guru-sys-blog-backend.vercel.app',
 ];
 
 export default allowedOrigins;
